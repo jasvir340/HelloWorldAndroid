9af8f16fbfe1f6ee3735c202d35425c3233b92cf
jasvir singh dhillon <jasvirdhillon340@gmail.com>
2020-02-23 03:31:47 +0530

    MainActivity Change

--- app/src/main/java/com/jvtech/helloworld/MainActivity.java
+++ app/src/main/java/com/jvtech/helloworld/MainActivity.java
@@ -2,12 +2,41 @@ package com.jvtech.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                String text = editText.getText().toString();
+                if(!text.isEmpty())
+                {
+                    textView.setText(text);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "No value entered !!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        
     }
 }
